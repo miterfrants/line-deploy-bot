b6120b32df7c2779d3c089801d12450dae2af2de
Peter Huang <miterfrnats@gmail.com>
2020-05-06 15:22:48 +0800

    feat: first commit

---
+++ Controllers/LineChatController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LineDeployBot.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class LineChatController : ControllerBase {
+
+        public LineChatController() {}
+
+        [HttpPost]
+        public ActionResult POST() {
+            string ChannelAccessToken = "LINE的TOKEN";
+
+            try {
+                return Ok();
+            } catch (Exception ex) {
+                return Ok();
+            }
+
+        }
+    }
+}
